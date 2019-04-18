6cb6dfc40d7d80ea693c8e087dea89d9c2ed386a
Johannes Huwald <=>
2019-04-18 12:40:40 +0200

    Added Code to render first triangle

--- apps/flower_seeds.cpp
+++ apps/flower_seeds.cpp
@@ -61,13 +61,36 @@ int main( void )
     .addVertexShader("resources/simple.vertexshader")
     .addFragmentShader("resources/simple.fragmentshader")
     .build();
+
+  GLuint vertexArrayID;
+  glGenVertexArrays(1, &vertexArrayID);
+  glBindVertexArray(vertexArrayID);
+
+  float h = sqrt(0.75f);
+  static const GLfloat g_vertex_buffer_data[] = {
+    -0.5f, -h, 0.0f,
+     0.5f, -h, 0.0f,
+     0.0f,  h, 0.0f,
+  };
+
+  GLuint vertexbuffer;
+  glGenBuffers(1, &vertexbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
   
 	do{
 		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
 		glClear( GL_COLOR_BUFFER_BIT );
 
-		// Draw nothing, see you in tutorial 2 !
+		glUseProgram(shader);
 
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
     
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -76,6 +99,11 @@ int main( void )
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
+
+  // Cleanup VBO
+  glDeleteBuffers(1, &vertexbuffer);
+  glDeleteVertexArrays(1, &vertexArrayID);
+  glDeleteProgram(shader);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
