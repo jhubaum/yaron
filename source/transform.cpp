6a88721723ca04ac033476af147a6d04cecc82f0
Johannes Huwald <=>
2019-04-21 20:44:09 +0200

    Fixed transform constructors

--- source/transform.cpp
+++ source/transform.cpp
@@ -6,14 +6,16 @@ Transform::Transform()
 {}
 
 Transform::Transform(const glm::vec3 &position)
+  : Transform(position, glm::quat(), {1.0f, 1.0f, 1.0f})
 {}
 
-Transform::Transform(const glm::vec3 &position, const glm::quat &rotation) {}
+Transform::Transform(const glm::vec3 &position, const glm::quat &rotation)
+  : Transform(position, rotation, {1.0f, 1.0f, 1.0f})
+{}
 
 Transform::Transform(const glm::vec3 &position, const glm::quat &rotation, const glm::vec3 &scale)
   : position(position), rotation(rotation), scale(scale)
-{
-}
+{}
 
 glm::mat4 Transform::calculateWorld() const {
   glm::mat4 world(1.0f);
